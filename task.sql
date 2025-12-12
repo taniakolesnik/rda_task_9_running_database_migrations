9bd21da3b59762eaea0a4ea31d9eb5f733e6b735
Tania Kolesnik <kolesnik@gmail.com>
2025-12-12 10:07:59 +0000

    update task.sql

--- task.sql
+++ task.sql
@@ -38,3 +38,18 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 --rollback DROP TABLE ProductInventory;
+
+--changeset mate.acamemy:5 labels:0.0.2
+CREATE TABLE Users (
+    ID INT,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+--rollback DROP TABLE Users;
+
+--changeset mate.acamemy:6 labels:0.0.3
+CREATE INDEX email_index
+ON Users (Email);
+--rollback DROP INDEX email_index ON Users;
